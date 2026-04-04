9cc876101c9c253ef580d2321bfcb0276ddb9cf4
Sang Jun Seo <126740159+20SSJ@users.noreply.github.com>
2026-04-04 19:08:37 +0900

    [Silver I] Title: 단지번호붙이기, Time: 80 ms, Memory: 11772 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -4,14 +4,15 @@ import java.util.*;
 public class Main {
 
 	static int[][] apt;
-	static int N, num, cnt;
+	static int N, cnt;
 	static int[] dr = {-1, 1, 0, 0};
 	static int[] dc = {0, 0, -1, 1};
 	static boolean v[][];
-	static void attach(int r, int c) {
-		apt[r][c] = num;
+	
+	static void attachAndCnt(int r, int c) {
 		Queue<int[]> q = new ArrayDeque<>();
 		q.offer(new int[] {r, c});
+		v[r][c] = true;
 		while(!q.isEmpty()) {
 			int[] cur = q.poll();
 			int curR = cur[0];
@@ -22,21 +23,9 @@ public class Main {
 				int nc = curC + dc[d];
 				if(isValid(nr, nc) && apt[nr][nc] == 1 && !v[nr][nc]) {
 					q.offer(new int[] {nr, nc});
-					apt[nr][nc] = num;
 					v[nr][nc] = true;
+					cnt++;
 				}
-			}
-		}
-	}
-	
-	public static void count(int r, int c) {
-		v[r][c] = true;
-		for(int d = 0; d < 4; d++) {
-			int nr = r + dr[d];
-			int nc = c + dc[d];
-			if(isValid(nr, nc) && !v[nr][nc] && apt[nr][nc] > 0) {
-				cnt++;
-				count(nr, nc);
 			}
 		}
 	}
@@ -45,52 +34,38 @@ public class Main {
 		return r >= 0 && r < N && c >= 0 && c < N;
 	}
 	
-	public static void print() {
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				System.out.print(apt[i][j] + " ");
-			}System.out.println();
-		}
-	}
-	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		apt = new int[N][N];
+		
 		for(int i = 0; i < N; i++) {
 			String str = br.readLine();
 			for(int j = 0; j < N; j++) {
 				apt[i][j] = str.charAt(j) - '0';
 			}
 		}
+		
 		v = new boolean[N][N];
-		num = 1;
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				if(apt[i][j] > 0 && !v[i][j]) {
-					attach(i, j);
-					num++;
-				}
-			}
-		}
-		num--;
-		System.out.println(num);
-		int idx = 0;
-		int arr[] = new int[num];
-		v = new boolean[N][N];
+		int num = 0;
+		ArrayList<Integer> arr = new ArrayList<>();
 		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < N; j++) {
 				if(apt[i][j] > 0 && !v[i][j]) {
 					cnt = 1;
-					count(i, j);
-					arr[idx++] = cnt;
+					attachAndCnt(i, j);
+					arr.add(cnt);
+					num++;
 				}
 			}
 		}
-		Arrays.sort(arr);
+		v = new boolean[N][N];
+		
+		Collections.sort(arr);
+		
+		System.out.println(num);
 		for(int  i = 0; i < num; i++) {
-			System.out.println(arr[i]);
+			System.out.println(arr.get(i));
 		}
 	}
-
 }
