700728cc98e05cc2e6fb2d0b6b444dc2e1f9ae9c
Sang Jun Seo <126740159+20SSJ@users.noreply.github.com>
2026-04-07 23:50:05 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 64 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		if(n < 2) {
+			System.out.println(n);
+			return;
+		}
+		
+		long fib[] = new long[n + 1];
+		fib[0] = 0;
+		fib[1] = 1;
+		for(int i = 2; i <= n; i++) {
+			fib[i] = fib[i - 1] + fib[i - 2];
+		}
+		System.out.println(fib[n]);
+	}
+}
