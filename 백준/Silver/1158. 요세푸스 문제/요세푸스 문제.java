caecc28386a39c923037292672659f1194f7049e
Sang Jun Seo <126740159+20SSJ@users.noreply.github.com>
2026-04-09 23:31:36 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 208 ms, Memory: 12992 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -11,20 +11,16 @@ public class Main {
 		int K = Integer.parseInt(st.nextToken());
 		
 		Queue<Integer> q = new ArrayDeque<>();
-		Queue<Integer> result = new ArrayDeque<>();
+		StringBuilder sb = new StringBuilder();
+		sb.append("<");
 		for(int i = 1; i <= N; i++) q.add(i);
 		while(q.size() > 1) {
 			for(int i = 1; i < K; i++) {
 				q.offer(q.poll());
 			}
-			result.add(q.poll());
+			sb.append(q.poll()).append(", ");
 		}
-		result.offer(q.poll());
-		StringBuilder sb = new StringBuilder();
-		while(result.size() > 1) {
-			sb.append(result.poll()).append(", ");
-		}
-		sb.append(result.poll());
-		System.out.println("<" + sb + ">");
+		sb.append(q.poll()).append(">");
+		System.out.println(sb);
 	}
 }
