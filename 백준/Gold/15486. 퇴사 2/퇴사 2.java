0cae7ea0d9d56bcae834e53c90c08b84ee8b4d2f
Sang Jun Seo <126740159+20SSJ@users.noreply.github.com>
2026-04-09 13:37:38 +0900

    [Gold V] Title: 퇴사 2, Time: 632 ms, Memory: 308156 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -3,26 +3,28 @@ import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int n = Integer.parseInt(br.readLine());
-		int dp[] = new int[n + 2];
-		int t[] = new int[n + 1];
-		int v[] = new int[n + 1];
-		for(int i = 1; i <= n; i++) {
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] t = new int[N + 1];
+		int[] v = new int[N + 1];
+		for(int i = 1; i <= N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			t[i] = Integer.parseInt(st.nextToken());
 			v[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		for(int i = n; i >= 1; i--) {
-			if(i + t[i] <= n + 1) {
-				dp[i] = Math.max(dp[i + t[i]] + v[i], dp[i + 1]);
-			} else {
-				dp[i] = dp[i + 1];
-			}
+		int[] dp = new int[N + 2]; // 4 
+		for(int i = 1; i <= N; i++) {
+			dp[i] = Math.max(dp[i], dp[i-1]);
+			if(i + t[i] <= N + 1) {
+				dp[i + t[i]] = Math.max(dp[i + t[i]], dp[i] + v[i]);
+			} 
 		}
-		System.out.println(dp[1]);
+		
+		int ans = Math.max(dp[N], dp[N + 1]);
+		System.out.println(ans);
 	}
 }
