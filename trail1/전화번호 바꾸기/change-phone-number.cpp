187ce48c82ef77fcf964890f7f41a4070b4df448
Sang Jun Seo <126740159+20SSJ@users.noreply.github.com>
2026-09-09 08:43:59 +0900

    [Passed] 전화번호 바꾸기 (2ms, 0MB)

---
+++ trail1/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r, m, e;
+    char d = '-';
+    cin >> r;
+    cin.get();
+    cin >> m;
+    cin.get();
+    cin >> e;
+    cout << 0 << r << d << e << d << m;
+    return 0;
+}
