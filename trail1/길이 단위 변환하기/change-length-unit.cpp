33223116643828f3a12a8ae2d015abeb8cafdf19
Sang Jun Seo <126740159+20SSJ@users.noreply.github.com>
2026-09-08 15:51:23 +0900

    [Passed] 길이 단위 변환하기 (2ms, 0MB)

---
+++ trail1/길이 단위 변환하기/change-length-unit.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float ft = 30.48;
+    int mi = 160934;
+    cout << fixed;
+    cout.precision(1);
+    cout << 9.2 << "ft = " << 9.2 * ft << "cm\n";
+    cout << 1.3 << "mi = " << 1.3 * mi << "cm"; 
+    return 0;
+}
