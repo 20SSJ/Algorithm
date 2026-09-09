d868fb6cfa1260f065811c79368d19c336929f1b
Sang Jun Seo <126740159+20SSJ@users.noreply.github.com>
2026-09-09 14:58:05 +0900

    [Passed] 남녀노소 구분짓기 (2ms, 0MB)

---
+++ trail1/남녀노소 구분짓기/sex-and-age.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int s, age;
+    cin >> s;
+    cin >> age;
+    if(s == 0){
+        if(age >= 19) cout << "MAN";
+        else cout << "BOY";
+    }else{
+        if(age >= 19) cout << "WOMAN";
+        else cout << "GIRL";
+    }
+    return 0;
+}
