bd2ea777d99234370496d376b51a34bcc70c21d5
Sang Jun Seo <126740159+20SSJ@users.noreply.github.com>
2026-09-16 08:58:57 +0900

    [Passed] 문자열 출력하기 (2ms, 0MB)

---
+++ trail1/문자열 출력하기/print-string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, i = 0;
+    cin >> N;
+    while(i < N){
+        cout << "LeebrosCode\n";
+        i++;
+    }
+    return 0;
+}
